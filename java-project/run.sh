6ae5b1b1a8fdde26304f9573d5a795206b08e6ed
GowthamGirithar <g.srirangasamy@sap.com>
2017-08-25 13:06:26 +0530

    added the files

--- java-project/run.sh
+++ java-project/run.sh
@@ -1,3 +1,3 @@
-javac -cp  .;/project/target/lib/weka.jar /project/target/src/main/java/com/gg/ml/LinearRegressionDemo.java
+javac -cp  /project/target/lib/weka.jar /project/target/src/main/java/com/gg/ml/LinearRegressionDemo.java
 
-java -cp  .;/project/target/lib/weka.jar /project/target/src/main/java/com/gg/ml/LinearRegressionDemo
+java -cp  /project/target/lib/weka.jar /src/main/java/com/gg/ml/LinearRegressionDemo
